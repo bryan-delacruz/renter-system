317ba181c4023c09bb101d9169238309373a6222
Bryan De La Cruz <bdelacruza@pucp.edu.pe>
2021-01-04 11:51:28 -0500

    cambios en el Model

--- renter/core/models.py
+++ renter/core/models.py
@@ -1,29 +1,61 @@
 from django.db import models
 
+
 class Owner(models.Model):
-    Name = models.CharField(max_length=50)
-    Surname = models.CharField(max_length=50)
-    dni = models.CharField(max_length=8)
-    adress = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    dni = models.CharField(max_length=8, unique=True)
+    adress = models.TextField()
     cellphone = models.CharField(max_length=9, blank=True, null=True)
     mail = models.EmailField(unique=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = '01-Propietarios'
+        ordering = ['name', 'surname']
+
+    def __str__(self):
+        return "{} {}, DNI: {}, Dirección: {}, Celular {}, Correo {}".format(self.name, self.surname, self.dni,
+                                                                          self.adress, self.cellphone, self.mail)
 
 
 class Renter(models.Model):
-    Name = models.CharField(max_length=50)
-    Surname = models.CharField(max_length=50)
-    dni = models.CharField(max_length=8)
-    adress = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    dni = models.CharField(max_length=8, unique=True)
+    adress = models.TextField()
     cellphone = models.CharField(max_length=9, blank=True, null=True)
     mail = models.EmailField(unique=True, blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = '02-Inquilinos'
+        ordering = ['name', 'surname']
 
-class Floor(models.Model):
-    Number = models.IntegerField(5)
+    def __str__(self):
+        return "{}, {} DNI: {} Dirección: {} Celular {} Correo {}".format(self.name, self.surname, self.dni,
+                                                                          self.adress, self.cellphone, self.mail)
 
 
 class Building(models.Model):
-    Name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = '03-Edificios'
+        ordering = ['name']
+
+    def __str__(self):
+        return "Edificio: {}".format(self.name)
+
+
+class Floor(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = '04-Pisos'
+        ordering = ['name']
+
+    def __str__(self):
+        return "Piso: {}".format( self.name)
+
 
 class Room(models.Model):
     owenerId = models.ForeignKey(Owner, on_delete=models.CASCADE)
@@ -43,5 +75,16 @@ class Room(models.Model):
     priceRoom = models.PositiveSmallIntegerField()
     priceSubTotal = models.PositiveSmallIntegerField(null=True, blank=True)
     priceTotal = models.PositiveSmallIntegerField()
+    priceMaintenance = models.PositiveSmallIntegerField(null=True, blank=True)
+    priceWarranty = models.PositiveSmallIntegerField(null=True, blank=True)
     dateInitial = models.DateField(null=True, blank=True)
     dateFinal = models.DateField(null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = '05-Habitaciones'
+        ordering = ['buildingId', 'floorId', 'renterId', 'priceTotal', 'dateInitial', 'dateFinal']
+
+    def __str__(self):
+        return "Edificio: {}, Piso: {}, Inquiligno: {}, Precio Total {}, fecha Inicial: {}, fecha Fin {}".format(
+            self.buildingId, self.floorId, self.renterId, self.priceTotal, self.dateInitial, self.dateFinal)
